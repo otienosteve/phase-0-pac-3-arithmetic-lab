d845eebe913b495baeede7de72f3c03b32e5fcc6
otienosteve <steveotieno701@gmail.com>
2022-03-30 14:37:27 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(n1,n2){return n1+n2}
+
+function subtract(n1,n2){return n1-n2;}
+
+function multiply(n1,n2){return n1*n2}
+
+function divide(n1,n2){return n1/n2}
+
+function increment(n) {n++; return n}
+
+function decrement(n){n--; return n} 
+
+function makeInt(n){return parseInt(n,10);}
+
+function preserveDecimal(n){return parseFloat(n)}
